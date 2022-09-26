a5fe6f8e7b7c1162ab311ce70199000b99108f67
Been98 <dudqls2078@naver.com>
2022-09-26 15:45:25 +0900

    9/26-2839

---
+++ baekjoon/2839.cpp
@@ -0,0 +1,27 @@
+//설탕 배달
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    int a = 0, b = 0;
+    cin >> num;
+    a = num / 5;
+    while(1){
+        if(a < 0){
+            cout <<"-1";
+            return;
+        }
+        if((num-(5*a))%3 == 0){
+            b = (num-(5*a)) / 3;
+            break;
+        }
+        a--;
+    }
+    cout << a+b;
+    return 0;
+}
