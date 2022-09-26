f5617d573235b736d2f8d066fdf3fce0d801b651
Been98 <dudqls2078@naver.com>
2022-09-26 15:17:13 +0900

    first commit

---
+++ programmers/Lv1_report.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> id_list, vector<string> report, int k)
+{
+    vector<int> answer;
+    vector<string, int> v;
+    
+    return answer;
+}
